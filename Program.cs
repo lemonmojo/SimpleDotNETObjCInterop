3b9bef55f2af4ac5b510ad24a2722d2f621ec89c
Felix Deimel <felix@lemonmojo.com>
2023-09-25 15:26:16 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleObjCInterop;
+
+class Program 
+{
+	public static int Main(string[] args)
+	{
+		const string unresolvedPathCS = "~/Downloads";
+		string resolvedPathCs = string.Empty;
+		
+		using (NSString unresolvedPathNS = new(unresolvedPathCS)) {
+			using (NSString? resolvedPathNS = unresolvedPathNS.StringByResolvingSymlinksInPath) {
+				if (resolvedPathNS is null) {
+					throw new Exception("Failed to resolve path");
+				}
+
+				resolvedPathCs = resolvedPathNS.UTF8String ?? string.Empty;
+			}
+		}
+		
+		Console.WriteLine($"Unresolved Path: {unresolvedPathCS}");
+		Console.WriteLine($"Resolved Path: {resolvedPathCs}");
+
+		return 0;
+	}
+}
